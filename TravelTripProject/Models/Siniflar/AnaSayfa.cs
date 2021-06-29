539404dfbff81b02ce22b4b61c158f276df53232
Salih Küçük <salihkucuk0@hotmail.com>
2021-06-29 11:21:59 +0300

    Add project files.

---
+++ TravelTripProject/Models/Siniflar/AnaSayfa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProject.Models.Siniflar
+{
+    public class AnaSayfa
+    {
+        [Key]
+        public int ID { get; set; }
+        public int Baslik { get; set; }
+        public int Aciklama { get; set; }
+    }
+}
